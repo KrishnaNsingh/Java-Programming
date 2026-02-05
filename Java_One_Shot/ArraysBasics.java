f0685a84114c9f6261211bc78f8caf213c3eda62
Krishna Narayan Singh <krishnanarayansingh65@gmail.com>
2026-02-05 23:29:17 +0530

    commit

---
+++ Java_One_Shot/ArraysBasics.java
@@ -0,0 +1,14 @@
+package Java_One_Shot;
+
+public class ArraysBasics {
+    public static void main(String[] args) {
+        int[] marks = new int[4];
+        marks[0] = 93;
+        marks[1] = 93;
+        marks[2] = 93;
+        //empty space return 0 or false if array in boolen 
+        System.out.println(marks[3]);
+        //length 
+        System.out.println(marks.length);
+    }
+}
---
+++ Java_One_Shot/Basics.java
@@ -0,0 +1,26 @@
+package Java_One_Shot;
+
+public class Basics {
+    public static void main(String[] args){
+        //bytes = 4  [-128 to 127]
+        //shorts = 2  [-32768 to 32767]
+        //ints = 4    [-2147483648 to 2147483647]
+        //longs = 8
+        //floats = 4
+        //doubles = 8
+        //chars = 2
+        //booleans = 1
+        // byte age = 30 ;
+        // int numb = 10 ; 
+        // long phoneNo = 1234567890L;
+        float pi = 3.14f ;
+        // double e = 2.71828 ;
+        // boolean isAdult = true ; 
+        // char grade = 'A' ; 
+        String name = "Alice" ; 
+        System.out.println(pi);
+
+        String name1 = name.replace('A', 'B');
+        System.out.println(name1);
+    }
+}
